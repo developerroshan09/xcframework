d468bb08c839bd7bbef0c1bbcc248fb1ea9eddb7
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2026-01-06 19:57:29 +0545

    ios

---
+++ release-to-spm.sh
@@ -0,0 +1,33 @@
+# ============================================
+# RELEASE CHECKLIST - ComposeApp iOS
+# ============================================
+
+VERSION="1.0.X"  # Change this each time
+
+# --- IN DEVELOPMENT REPO (xcframework) ---
+cd ~/xcframework
+./gradlew :composeApp:assembleComposeAppReleaseXCFramework
+cd composeApp/build/XCFrameworks/release
+zip -r ComposeApp.xcframework.zip ComposeApp.xcframework
+xcrun swift package compute-checksum ComposeApp.xcframework.zip
+# ☝️ COPY THE CHECKSUM
+
+# --- ON GITHUB (ComposeApp-iOS repo) ---
+# 1. Go to: github.com/USERNAME/ComposeApp-iOS/releases/new
+# 2. Tag: v1.0.X
+# 3. Upload: ComposeApp.xcframework.zip
+# 4. Publish
+
+# --- IN DISTRIBUTION REPO (ComposeApp-iOS) ---
+cd ~/ComposeApp-iOS
+# Edit Package.swift:
+#   - Change URL to: .../v1.0.X/ComposeApp.xcframework.zip
+#   - Change checksum to: "PASTE_CHECKSUM_HERE"
+
+git add Package.swift
+git commit -m "Release v1.0.X"
+git push
+git tag v1.0.X
+git push origin v1.0.X
+
+# DONE! 🎉
